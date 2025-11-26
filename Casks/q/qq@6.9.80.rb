c033b6817150621be31a5d03ebf8157c875a4c22
edgeworthlauzero <edgeworthlau@outlook.com>
2025-11-26 16:56:49 +0800

    Add Casks/q/qq@6.9.80.rb Fix QQ version to 6.9.80

---
+++ Casks/q/qq@6.9.80.rb
@@ -0,0 +1,36 @@
+cask "qq@6.9.80" do
+  version "6.9.80_250904_01"
+  sha256 "5548e059023169b8d7917261c5089026560391b2c334b69541e45a678586388b"
+
+  url "https://dldir1v6.qq.com/qqfile/qq/QQNT/Mac/QQ_#{version}.dmg"
+  name "QQ"
+  desc "Instant messaging tool"
+  homepage "https://im.qq.com/macqq/index.shtml"
+
+  livecheck do
+    url "https://cdn-go.cn/qq-web/im.qq.com_new/latest/rainbow/macOSConfig.js"
+    regex(/QQ[._-]v?(\d+(?:[._]\d+)+)\.dmg/i)
+  end
+
+  auto_updates true
+
+  app "QQ.app"
+
+  uninstall quit: "com.tencent.qq"
+
+  zap trash: [
+    "~/Library/Application Scripts/com.tencent.qq",
+    "~/Library/Application Scripts/FN2V63AD2J.com.tencent.localserver2",
+    "~/Library/Application Scripts/FN2V63AD2J.com.tencent.ScreenCapture2",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.tencent.qq.sfl*",
+    "~/Library/Caches/com.tencent.qq",
+    "~/Library/Containers/com.tencent.qq",
+    "~/Library/Containers/com.tencent.qq.share",
+    "~/Library/Containers/FN2V63AD2J.com.tencent.localserver2",
+    "~/Library/Containers/FN2V63AD2J.com.tencent.ScreenCapture2",
+    "~/Library/Group Containers/FN2V63AD2J.com.tencent",
+    "~/Library/Preferences/com.tencent.qq.plist",
+    "~/Library/Saved Application State/com.tencent.qq.savedState",
+    "~/Library/WebKit/com.tencent.qq",
+  ]
+end
